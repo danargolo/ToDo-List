5c3f8e558fd42cb262a8cb5cfc145179dea733ea
Danilo Argolo <dan_argolo@hotmail.com>
2022-09-06 15:18:04 -0300

    Resolvendo quesito 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const addTask = document.getElementById('texto-tarefa');
+const list = document.getElementById('lista-tarefas');
+
+
+
+const button = document.createElement("button");
+button.setAttribute('type', 'button');
+button.id = 'criar-tarefa';
+document.body.appendChild(button);
+
+
+button.addEventListener('click', () => {
+    const task = document.createElement('li');
+    list.appendChild(task);
+    task.innerText = addTask.value;
+    addTask.value = '';
+});
