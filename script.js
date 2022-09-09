960a3ddd9a077bc688127986835ae2e56dcae601
Danilo Argolo <dan_argolo@hotmail.com>
2022-09-09 14:58:33 -0300

    Resolvendo quesito 12 e refatora Buttons

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
 const addTask = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
 
-const btn = document.createElement('button');
-btn.setAttribute('type', 'button');
-btn.id = 'criar-tarefa';
-btn.innerText = 'Criar tarefa';
-document.body.appendChild(btn);
-
-const clearBtn = document.createElement('button');
-clearBtn.setAttribute('type', 'button');
-clearBtn.id = 'apaga-tudo';
-clearBtn.innerText = 'Apagar';
-document.body.appendChild(clearBtn);
-
-const clearCompletedBtn = document.createElement('button');
-clearCompletedBtn.setAttribute('type', 'button');
-clearCompletedBtn.id = 'remover-finalizados';
-clearCompletedBtn.innerText = 'Finalizados';
-document.body.appendChild(clearCompletedBtn);
-
-const saveBtn = document.createElement('button');
-saveBtn.setAttribute('type', 'button');
-saveBtn.id = 'salvar-tarefas';
-saveBtn.innerText = 'Salvar tarefas';
-document.body.appendChild(saveBtn);
+// Botões
+const btn = document.getElementById('criar-tarefa');
+const clearBtn = document.getElementById('apaga-tudo');
+const clearCompletedBtn = document.getElementById('remover-finalizados');
+const saveBtn = document.getElementById('salvar-tarefas');
+const clearSelectedBtn = document.getElementById('remover-selecionado');
 
 const listItens = document.getElementsByTagName('li');
-
 
 function selector(a) {
   for (let index = 0; index < listItens.length; index += 1) {
@@ -38,7 +20,7 @@ function selector(a) {
 function insert() {
   const task = document.createElement('li');
   task.innerText = addTask.value;
-  task.classList.add('normal');
+  task.classList.add();
   list.appendChild(task);
   tasker();
   checker();
@@ -48,13 +30,11 @@ btn.addEventListener('click', () => {
   insert();
   // saveTask.push({task: addTask.value});
   addTask.value = '';
-})
-
+});
 
 function completedOrNot(r) {
   if (r.target.classList.contains('completed')) {
     r.target.classList.remove('completed', 'selected');
-    // r.target.classList.remove('selected');
   } else {
     r.target.classList.add('completed');
     r.target.classList.remove('selected');
@@ -79,11 +59,11 @@ function save() {
     saveTask.push({
       status: listItens[index].className,
       task: listItens[index].innerText,
-    })}
-  localStorage.setItem('saveList', JSON.stringify(saveTask));
-};
+    });
+  } localStorage.setItem('saveList', JSON.stringify(saveTask));
+}
 
-saveBtn.addEventListener('click', save)
+saveBtn.addEventListener('click', save);
 
 function clear() {
   document.querySelector('ol').innerHTML = '';
@@ -101,30 +81,21 @@ function clearCompleted() {
 
 clearCompletedBtn.addEventListener('click', clearCompleted);
 
-
-const loadItens = JSON.parse(localStorage.getItem('saveList'));
 function load() {
-  const task = document.createElement('li');
+  const loadItens = JSON.parse(localStorage.getItem('saveList'));
   if (localStorage.saveList) {
-    for(let index = 0; index < loadItens.length; index += 1) {
+    for (let index = 0; index < loadItens.length; index += 1) {
+      const task = document.createElement('li');
       task.innerText = loadItens[index].task;
       task.className = loadItens[index].status;
+      list.appendChild(task);
     }
   }
-  list.appendChild(task);
 }
-// load();
-  
-const clearSelectedBtn = document.createElement('button');
-clearSelectedBtn.setAttribute('type', 'button');
-clearSelectedBtn.id = 'remover-selecionado';
-clearSelectedBtn.innerText = 'Remover Selecionado';
-document.body.appendChild(clearSelectedBtn);
+window.addEventListener('load', load);
 
 function clearSelected() {
   const select = document.querySelector('.selected');
   list.removeChild(select);
 }
 clearSelectedBtn.addEventListener('click', clearSelected);
-
-
